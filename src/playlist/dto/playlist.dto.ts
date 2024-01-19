5e0e90d2d9ced62bad80a709690be8193f50c189
wynadiis23 <adii.empty@gmail.com>
2024-01-19 11:44:27 +0800

    (swagger): update documentation in playlist

--- src/playlist/dto/playlist.dto.ts
+++ src/playlist/dto/playlist.dto.ts
@@ -1,7 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class PlaylistDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    name: 'name',
+    type: 'string',
+    required: true,
+    description: 'Playlist name',
+    example: 'playlist 17 august',
+  })
   name: string;
 }
