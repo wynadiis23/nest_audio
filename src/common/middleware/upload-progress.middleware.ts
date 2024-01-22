4e84fd94058121dbbbcf88a61fd79a1c2a537aac
wynadiis23 <adii.empty@gmail.com>
2024-01-22 09:36:38 +0800

    (upload-track): add event listener and sse endpoint for upload progess and saving tracks

---
+++ src/common/middleware/upload-progress.middleware.ts
@@ -0,0 +1,25 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { NextFunction, Request } from 'express';
+
+@Injectable()
+export class UploadProgressMiddleware implements NestMiddleware {
+  constructor(private eventEmitter: EventEmitter2) {}
+
+  use(req: Request, res: Response, next: NextFunction) {
+    let progress = 0;
+    const fileSize = parseInt(req.headers['content-length']);
+
+    req.on('data', (chunk) => {
+      progress += chunk.length;
+      const percentage = (progress / fileSize) * 100;
+
+      this.eventEmitter.emit('upload-progress', {
+        uploadProgress: percentage,
+        totalFileSize: fileSize,
+      });
+    });
+
+    next();
+  }
+}
