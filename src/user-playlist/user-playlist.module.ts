b505231627c23e85254e0a122fd078f790120379
wynadiis23 <adii.empty@gmail.com>
2024-01-26 13:57:54 +0800

    (user-specified-playlist): add endpoint to return list of not added yet for playlist

--- src/user-playlist/user-playlist.module.ts
+++ src/user-playlist/user-playlist.module.ts
@@ -4,9 +4,10 @@ import { UserPlaylistService } from './user-playlist.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserPlaylist } from './entity/user-playlist.entity';
 import { Playlist } from '../playlist/entity/playlist.entity';
+import { User } from '../user/entity/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserPlaylist, Playlist])],
+  imports: [TypeOrmModule.forFeature([UserPlaylist, Playlist, User])],
   controllers: [UserPlaylistController],
   providers: [UserPlaylistService],
 })
