df42aaaac824eae085df482fb94ca327d89a6350
wynadiis23 <adii.empty@gmail.com>
2024-01-29 09:33:48 +0800

    (user): add update and remove user

---
+++ src/user/enum/is-active.enum.ts
@@ -0,0 +1,4 @@
+export enum IsActiveEnum {
+  INACTIVE = '0',
+  ACTIVE = '1',
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,15 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
-import { Repository } from 'typeorm';
+import { DeepPartial, Repository } from 'typeorm';
 import { UserRole } from '../user-role/entity/user-role.entity';
 import { UserPlaylist } from '../user-playlist/entity/user-playlist.entity';
 import { Playlist } from '../playlist/entity/playlist.entity';
+import { UpdateUserDto } from './dto';
 
 @Injectable()
 export class UserService {
@@ -75,6 +80,56 @@ export class UserService {
           id,
         },
       });
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async update(id: string, payload: UpdateUserDto) {
+    try {
+      if (id !== payload.id) {
+        throw new BadRequestException('invalid user id');
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { roles, ...rest } = payload;
+      const updateUser: DeepPartial<User> = { ...rest };
+
+      const userRoles: UserRole[] = [];
+      if (payload?.roles?.length) {
+        for (const role of payload.roles) {
+          const userRole = new UserRole();
+          userRole.code = role;
+          userRole.userId = id;
+
+          userRoles.push(userRole);
+        }
+
+        updateUser.roles = userRoles;
+      }
+
+      if (payload.isActive) {
+        updateUser.isActive = payload.isActive;
+      }
+
+      const data = this.userRepository.create({
+        id,
+        ...updateUser,
+      });
+
+      await this.userRepository.save(data);
+    } catch (error) {
+      console.log(error);
+      if (error instanceof BadRequestException) {
+        throw new BadRequestException(error.message);
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async remove(id: string) {
+    try {
+      await this.userRepository.delete(id);
     } catch (error) {
       throw new InternalServerErrorException();
     }
