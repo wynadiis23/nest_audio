bd0b7bab979a0bc4bc0867cd0ad2e3dc1b88cc0a
wynadiis23 <adii.empty@gmail.com>
2024-02-26 17:12:15 +0800

    fix(logger): update pagination logger service. there's still bug here

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectConnection } from '@nestjs/mongoose';
-import { Connection, Types } from 'mongoose';
+import { Connection, ObjectId, Types, SortOrder } from 'mongoose';
 import { LogQueryInterface } from './interface';
 
 @Injectable()
@@ -25,11 +25,24 @@ export class LoggerService {
         };
       }
 
-      const response = await x.find(query).toArray();
+      const { paginatedQuery, nextKeyFn } = this.generatePaginationQuery(
+        query,
+        [],
+        logQueryOptions.cursor ? logQueryOptions.cursor : null,
+      );
 
-      console.log(response.length);
+      console.log(JSON.stringify(paginatedQuery, null, 2));
+      console.log('next key function', nextKeyFn);
 
-      return response;
+      const response = await x.find(paginatedQuery).limit(5).toArray();
+
+      if (response.length) {
+        const nextKey = nextKeyFn(response);
+
+        return [response, nextKey._id.toString()];
+      }
+
+      return [response, null];
     } catch (error) {
       console.log(error);
       throw new InternalServerErrorException();
@@ -49,5 +62,68 @@ export class LoggerService {
     } catch (error) {
       throw new InternalServerErrorException();
     }
+  }
+
+  generatePaginationQuery(query, sort, nextKey) {
+    const sortField = sort == null ? null : sort[0];
+    console.log(sortField);
+
+    function nextKeyFn(items) {
+      if (items.length === 0) {
+        return null;
+      }
+
+      const item = items[items.length - 1];
+
+      if (sortField == null) {
+        console.log('sortfield null');
+        return { _id: item._id };
+      }
+
+      console.log([sortField]);
+      console.log(item[sortField]);
+
+      return { _id: item._id, [sortField]: item[sortField] };
+    }
+
+    if (nextKey == null) {
+      return { paginatedQuery: query, nextKeyFn };
+    }
+
+    let paginatedQuery = query;
+
+    if (sort == null) {
+      paginatedQuery._id = { $gt: nextKey._id };
+      return { paginatedQuery, nextKey };
+    }
+
+    const sortOperator = sort[1] === 1 ? '$gt' : '$lt';
+
+    const paginationQuery = [
+      // { [sortField]: { [sortOperator]: nextKey[sortField] } },
+      {
+        $and: [
+          // { [sortField]: nextKey[sortField] },
+          { _id: { $gt: nextKey._id } },
+        ],
+      },
+    ];
+
+    paginatedQuery = {
+      $and: [
+        query,
+        { _id: { $gt: Types.ObjectId.createFromHexString(nextKey) } },
+      ],
+    };
+
+    // if (paginatedQuery.$or == null) {
+    //   console.log('dinis');
+    //   paginatedQuery.$or = paginationQuery;
+    // } else {
+    //   console.log('masuk sini');
+
+    // }
+
+    return { paginatedQuery, nextKeyFn };
   }
 }
