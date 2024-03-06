400f1709f6ae5bbcb887292f96b5217bf5e5d463
wynadiis23 <adii.empty@gmail.com>
2024-03-06 17:35:35 +0800

    feat(users): add new endpoint to add client key on user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { LastActivity } from '../last-activity/entity/last-activity.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, LastActivity])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
