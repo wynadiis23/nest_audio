f5b7e11a32de9a156d1114ac64a370d655bb4215
wynadiis23 <adii.empty@gmail.com>
2024-03-08 12:13:26 +0800

    refactor(ioredis): change cache manager/module to ioredis

--- src/redis-cache/redis-cache.module.ts
+++ src/redis-cache/redis-cache.module.ts
@@ -3,19 +3,30 @@ import { RedisCacheService } from './redis-cache.service';
 import * as redisStore from 'cache-manager-redis-store';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
   imports: [
-    CacheModule.registerAsync({
+    // CacheModule.registerAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: async (configService: ConfigService) => ({
+    //     store: redisStore,
+    //     url: `${configService.get<string>('REDIS_URL')}?family=0`,
+    //     // host: configService.get<string>('REDIS_HOST'),
+    //     // port: configService.get<number>('REDIS_PORT'),
+    //     ttl: configService.get<number>('REDIS_TTL'),
+    //     // password: configService.get<string>('REDIS_PASSWORD'),
+    //   }),
+    //   isGlobal: true,
+    // }),
+    RedisModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        store: redisStore,
-        host: configService.get<string>('REDIS_HOST'),
-        port: configService.get<number>('REDIS_PORT'),
-        ttl: configService.get<number>('REDIS_TTL'),
+        type: 'single',
+        url: `${configService.get<string>('REDIS_URL')}?family=0`,
       }),
-      isGlobal: true,
     }),
   ],
   providers: [RedisCacheService],
